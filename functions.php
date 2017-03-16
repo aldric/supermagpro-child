56295a5ea7b97e1d1263f35cd9edd9c2306eda9e
a.gaudinot <gaudinot@gmail.com>
2017-03-16 17:46:17 +0100

    Card styling / adding vue not using it

--- functions.php
+++ functions.php
@@ -21,11 +21,19 @@ function suprmagpro_child_enqueue_styles()
     wp_register_script('tether-js', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array('jquery'), null, true);
     wp_register_style('bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css', false, null, 'all');
 
+    // if (defined('WP_DEBUG') && true === WP_DEBUG) {
+    //     wp_register_script('vue-js', $template_directory . '/assets/js/vue.js', array(), null, true);
+    // } else {
+    //     wp_register_script('vue-js', $template_directory . '/assets/js/vue.min.js', array(), null, true);
+    // }
+
     wp_register_script('main-js', $template_directory.'/main.js', null, null, true);
 
     wp_enqueue_script('bootstrap-js');
     wp_enqueue_script('tether-js');
     wp_enqueue_script('main-js');
+    wp_enqueue_script('vue-js');
+    
     wp_enqueue_script('custom-agt-js');
 
     wp_enqueue_style('bootstrap-css');
@@ -33,6 +41,21 @@ function suprmagpro_child_enqueue_styles()
     wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_enqueue_style('supermagpro-child-style', $template_directory. '/style.css', array( $parent_style ));
 }
+
+// function endsWith($haystack, $needle)
+// {
+//     $length = strlen($needle);
+//     if ($length == 0) {
+//         return true;
+//     }
+//     return (substr($haystack, -$length) === $needle);
+// }
+
+// function defer_parsing_of_js ( $url ) {
+//     return endsWith($url, "vue.js") || endsWith($url, "vue.min.js") ? "$url' defer " : $url;
+// }
+
+// add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
 
 add_action('wp_enqueue_scripts', 'suprmagpro_child_enqueue_styles', 15);
 
