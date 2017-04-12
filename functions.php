63e9f03812c067b9ba3440f92def93ad521e666b
a.gaudinot <gaudinot@gmail.com>
2017-04-12 17:51:50 +0200

    style css dequeue requeue

--- functions.php
+++ functions.php
@@ -9,32 +9,28 @@ function project_dequeue_unnecessary_scripts()
     wp_dequeue_style('fontawesome');
     wp_deregister_script('supermag-custom');
 }
-add_action('wp_print_scripts', 'project_dequeue_unnecessary_scripts');
+
+add_action('wp_enqueue_scripts', 'project_dequeue_unnecessary_scripts',100);
 
 function suprmagpro_child_enqueue_styles()
 {
     $template_directory = get_stylesheet_directory_uri();
 
     wp_register_script('custom-agt-js', $template_directory . '/assets/js/supermag-custom.js', array('jquery'), '4.0', 1);
-
     wp_register_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, true);
-    //wp_register_script('tether-js', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array('jquery'), null, true);
     wp_register_style('bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', false, null, 'all');
     wp_register_script('fontawesome-async', '//use.fontawesome.com/f93bfa54b2.js', null, '4.7.0');
 
-    // if (defined('WP_DEBUG') && true === WP_DEBUG) {
-    //     wp_register_script('vue-js', $template_directory . '/assets/js/vue.js', array(), null, true);
-    // } else {
-    //     wp_register_script('vue-js', $template_directory . '/assets/js/vue.min.js', array(), null, true);
-    // }
+    if (defined('WP_DEBUG') && true === WP_DEBUG) {
+        wp_register_script('vue-js', $template_directory . '/assets/js/vue.js', array(), null, true);
+    } else {
+        wp_register_script('vue-js', $template_directory . '/assets/js/vue.min.js', array(), null, true);
+    }
     wp_register_script('main-js', $template_directory.'/main.js', null, null, true);
 
     wp_enqueue_script('bootstrap-js');
-  //  wp_enqueue_script('tether-js');
-    //wp_enqueue_script('main-js');
     wp_enqueue_script('fontawesome-async');
-    //wp_enqueue_script('vue-js');
-
+    wp_enqueue_script('vue-js');
     wp_enqueue_script('custom-agt-js');
     wp_enqueue_style('bootstrap-css');
     $parent_style = 'supermagpro-parent-style';
