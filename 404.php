bb4c33d854892231d5f85d5a4d33b81106651e10
a.gaudinot <gaudinot@gmail.com>
2017-04-21 09:53:46 +0200

    404 links

--- 404.php
+++ 404.php
@@ -23,7 +23,13 @@ get_header(); ?>
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'supermag' ); ?></p>
 
 					<?php get_search_form(); ?>
-
+					<div class="list-group">
+					  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="list-group-item">
+					    Retourner &agrave; l'accueil
+					  </a>
+					  <a href="<?php echo esc_url( home_url( '/banque-en-ligne/' ) ); ?>" class="list-group-item">Comparer les banques, et trouver celle qui vous convient</a>
+					  <a href="<?php echo esc_url( home_url( '/contactez-nos-experts/' ) ); ?>" class="list-group-item">Contacter notre &eacute;quipe</a>
+					</div>
 					<div style="padding-bottom:50px;"><img src="<?php echo get_stylesheet_directory_uri().'/404.png'; ?>"></img></div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
