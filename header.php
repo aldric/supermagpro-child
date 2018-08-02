320470c03416fe4b9747d904e1509762bee718b6
Aldric Gaudinot <aldric@bluewindowltd.com>
2018-08-02 23:25:50 +0200

    gdpr / GTM

--- header.php
+++ header.php
@@ -20,6 +20,13 @@
 do_action( 'supermag_action_before_head' );?>
 	<head>
 	<!-- TradeDoubler site verification 2973410 -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MDJWJQ3');</script>
+<!-- End Google Tag Manager -->
 		<?php
 		/**
 		 * supermag_action_before_wp_head hook
@@ -41,7 +48,10 @@ do_action( 'supermag_action_before_head' );?>
  * @hooked supermag_body_attr- 10
  */
 do_action( 'supermag_action_body_attr' );?>>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDJWJQ3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 /**
  * supermag_action_before hook
